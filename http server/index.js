fa8811afe7acbf8b907f8b04bcf43208a1e1cb15
uzairahmed2126 <uzairahmed21261@gmail.com>
2025-07-16 22:19:59 +0530

    http server log file

---
+++ http server/index.js
@@ -0,0 +1,13 @@
+import http from  "http";
+import  fs from "fs";
+const  port= 9001;
+const server = http.createServer((req,res)=>{
+    const log = `${new Date()} new request recevied\n`
+    fs.appendFile("log.txt",log,(err)=>{
+        if(err) {
+            console.log("error while append file",err);
+        }
+        res.end("Hello from server");
+    })
+});
+server.listen(port,()=>console.log(`server listen port :${port}` ))
